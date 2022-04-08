4dfd2af838f61ba5cfc8e0e997bee3105bd6cb6e
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-08 13:08:53 +0200

    route on submit

--- src/views/EditAnimalView.vue
+++ src/views/EditAnimalView.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import { RouterLink, useRoute } from 'vue-router'
+import { RouterLink, useRoute } from "vue-router";
 import AddAnimalForm from "../components/AddAnimalForm.vue";
 import AnimalList from "../components/AnimalList.vue";
 import EditAnimalForm from "../components/EditAnimalForm.vue";
@@ -13,24 +13,28 @@ export interface Animal {
 }
 
 export default defineComponent({
-    setup() {
-        const route = useRoute()
-        const animal = ref<Animal>();
-        async function fetchData() {
-            const result = await fetch(`http://localhost:9999/animal/${route.params.id}/`);
-            animal.value = await result.json();
-        }
-        onMounted(() => {
-            fetchData();
-        });
-        return { animal, fetchData };
-    },
-    components: { AddAnimalForm, AnimalList, EditAnimalForm }
+  setup() {
+    const route = useRoute();
+    const animal = ref<Animal>();
+    async function fetchData() {
+      const result = await fetch(
+        `http://localhost:9999/animal/${route.params.id}/`
+      );
+      animal.value = await result.json();
+    }
+    onMounted(() => {
+      fetchData();
+    });
+    return { animal, fetchData };
+  },
+  components: { AddAnimalForm, AnimalList, EditAnimalForm },
 });
 </script>
 
 <template>
-<div class="content">
-<EditAnimalForm v-if='animal' :animal="animal"/>
-</div>
+  <div class="content">
+    <div class="paddedContainer">
+      <EditAnimalForm v-if="animal" :animal="animal" />
+    </div>
+  </div>
 </template>
