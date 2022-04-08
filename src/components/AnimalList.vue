3a600eedb60fb8142f86fe876a4ae212b5c997a0
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-04-08 13:09:08 +0200

    only show edit if admin

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import type { Animal } from "@/views/HomeView.vue";
 import { defineProps, ref } from "vue";
+import { useRoute } from "vue-router";
 
 interface Props {
   animals: Animal[];
 }
-
 const { animals } = defineProps<Props>();
+const route = useRoute();
+
+const isAdmin = route.path.includes("/admin");
 </script>
 
 <template>
   <h2>All animals available:</h2>
-  <ul>
-    <li v-for="animal in animals">
+  <ul class="gridBox">
+    <li v-for="animal in animals" class="smallAnimalCard">
       <h3>{{ animal.name }}</h3>
-      <p>id: {{ animal.id }}</p>
-      <p>dangerous: {{ animal.dangerous }}</p>
-      <p>fur-type: {{ animal.fur }}</p>
-      <button class="iconButton">
-        <RouterLink :to="`/animal/${animal.id}/edit`"> Edit </RouterLink>
-      </button>
+      <div class="rowList">
+        <img src="@/assets/animals/1.png" />
+        <!-- <img src="src\assets\animals\${1.png}`"> -->
+        <div class="columnList">
+          <p>id: {{ animal.id }}</p>
+          <p>dangerous: {{ animal.dangerous }}</p>
+          <p>fur-type: {{ animal.fur }}</p>
+          <RouterLink
+            :to="`/animal/${animal.id}/edit`"
+            class="button"
+            v-if="isAdmin"
+          >
+            Edit
+          </RouterLink>
+        </div>
+      </div>
     </li>
   </ul>
 </template>
-
